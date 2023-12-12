62b0f589a4a21f5f2aa595994950a3e8d0bf530a
JorgeKlisyman <132587757+JorgeKlisyman@users.noreply.github.com>
2023-12-12 13:20:03 -0300

    finalizei todos os trabalho da conta bancaria, reajustei tudo que tava faltando

--- contaPoupanca.js
+++
@@ -1,7 +0,0 @@
- import { ContaBancaria } from "./contaBancaria";
- 
- export class ContaPoupanca extends ContaBancaria {
-    constructor(agencia, numero, saldo) {
-        super(agencia, numero, 'poupanca', saldo);
-    }
-}
